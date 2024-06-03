77b3e51998467957a2f48875a032e544bcb7d268
rheanne <iya.rheanne@gmail.com>
2024-06-03 14:06:46 -0700

    Update login.js

--- login.js
+++ login.js
@@ -13,12 +13,15 @@ window.onclick = function(event) {
 var password = "06/20/2022";
 
 function passcheck() {
-    if(document.getElementById('pass1').value != password) {
-        alert("Wow...you don't know our anniversary :c. Press Ok to CONTINUE")
+    var userInput = document.getElementById('pass1').value;
+
+    if(userInput !== password) {
+        alert("Wow...you don't know our anniversary :c. Press Ok to TRY AGAIN")
         return false;
     }
 
-    if(document.getElementById('pass1').value == password) {
+    if(userInput === password) {
         alert("YAY GOOD JOB MY LOVE! Click Ok to CONTINUE")
+        return true;
     }
 }
